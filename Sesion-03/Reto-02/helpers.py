089cf17e8f0bed860e3316bf6279e0e29f8c73a8
Manuel Soto Romero <manu@ciencias.unam.mx>
2023-10-11 21:05:07 -0600

    Update helpers.py

--- Sesion-03/Reto-02/helpers.py
+++ Sesion-03/Reto-02/helpers.py
@@ -3,4 +3,4 @@ def crear_histograma_con(datos):
     import seaborn as sns
     import numpy as np
     
-    sns.distplot(datos, kde=False, bins=len(np.unique(datos)))
+    sns.histplot(datos, kde=False, bins=len(np.unique(datos)))
